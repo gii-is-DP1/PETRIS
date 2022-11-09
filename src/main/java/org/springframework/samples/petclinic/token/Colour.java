ef134a80e735cdda7856eb5d4dc9e45fbbaa9c20
gonriblun <gonriblun@alum.us.es>
2022-11-09 13:56:32 +0100

    Creacion Token, Player y Space

---
+++ src/main/java/org/springframework/samples/petclinic/token/Colour.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic.token;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Colour extends BaseEntity{
+
+    @NotNull
+    String name; 
+
+}

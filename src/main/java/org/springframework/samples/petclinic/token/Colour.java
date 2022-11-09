1add8a30b229e638829dac26293c1075d1771c34
gonriblun <gonriblun@alum.us.es>
2022-11-09 18:47:37 +0100

    Arreglo sql

--- src/main/java/org/springframework/samples/petclinic/token/Colour.java
+++ src/main/java/org/springframework/samples/petclinic/token/Colour.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.token;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "colours")
 public class Colour extends BaseEntity{
 
     @NotNull
--- src/main/java/org/springframework/samples/petclinic/token/Token.java
+++ src/main/java/org/springframework/samples/petclinic/token/Token.java
@@ -1,26 +1,30 @@
 package org.springframework.samples.petclinic.token;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.player.Player;
 import org.springframework.samples.petclinic.space.Space;
 
 @Entity
 @Table(name = "tokens")
 public class Token extends BaseEntity{
-
-    @Column(name= "token_type")       
+      
 	@NotNull
+	@ManyToOne
 	private TokenType tokenType;
-
-	@Column(name = "colour")        
+       
 	@NotNull
+	@ManyToOne
 	private Colour colour;
 
-	@OneToOne(optional = true)
+	@ManyToOne(optional = true)
 	private Space space;
+
+	@ManyToOne
+	@NotNull
+	private Player player;
 }

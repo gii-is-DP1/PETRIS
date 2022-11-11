caa1efd258794c689f8cf2ad9af6ecae2ff49821
dancorfon@us.es <dancorfon@us.es>
2022-11-11 18:46:58 +0100

    Login personalizado

--- src/main/java/org/springframework/samples/petclinic/web/LoginController.java
+++ src/main/java/org/springframework/samples/petclinic/web/LoginController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    
-
     @GetMapping("/login")
     public String login(){
         return "/users/loginForm";

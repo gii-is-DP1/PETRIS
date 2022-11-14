a1ba9b27ffcc2a379dc4c87be7d0111c77531a38
gonriblun <gonriblun@alum.us.es>
2022-11-14 01:35:09 +0100

    game create actualization

--- src/main/java/org/springframework/samples/petclinic/user/User.java
+++ src/main/java/org/springframework/samples/petclinic/user/User.java
@@ -8,14 +8,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.player.Player;
 
 import lombok.Getter;

3b3815745df32accee082aa0aff366b8712b9928
LUCAS ANTOÑANZAS <lucantdel@alum.us.es>
2022-11-17 00:39:16 +0100

    limpia achievements

--- src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
+++ src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
@@ -1,22 +1,11 @@
-/*
 package org.springframework.samples.petclinic.achievements;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AchievementRepository extends CrudRepository<Achievement, Integer> {
 
-    List<Achievement> findAll();
-    /*
-    @Query("SELECT u FROM User u WHERE u.userId=:userId")
-    public List<Achievement> findPlayerAchievements(String userId);
     
-    public Achievement findByName(String name);
     
 }
-*/

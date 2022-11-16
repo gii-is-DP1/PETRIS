f621e13fa1c9e71e0c0234355c31e20bec2cf1a5
gonriblun <gonriblun@alum.us.es>
2022-11-16 12:18:03 +0100

    generateGameCode

--- src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.user;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;

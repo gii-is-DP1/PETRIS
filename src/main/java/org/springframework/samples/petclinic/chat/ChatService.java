3eaa0e1c5eb530a10b310773440f38eef108e0c3
ferbaqfer <ferbaqfer@alum.us.es>
2022-11-28 20:10:12 +0100

    Chat semi funcional

--- src/main/java/org/springframework/samples/petclinic/chat/ChatService.java
+++ src/main/java/org/springframework/samples/petclinic/chat/ChatService.java
@@ -1,8 +1,31 @@
 package org.springframework.samples.petclinic.chat;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ChatService {
+
+    @Autowired 
+    ChatRepository chatRepository;
+
+    @Autowired
+	public ChatService(ChatRepository chatRepository) {
+		this.chatRepository = chatRepository;
+	}
+
+    @Transactional
+    public void saveChat(Chat chat) {
+       chatRepository.save(chat);
+    }
+
+    @Transactional(readOnly = true)
+	public Collection<Chat> getChatsById(Integer id) throws DataAccessException {
+		return chatRepository.findChatsById(id);
+	}
     
 }

e09b8d80d29932a4520b7eb196e4197c83e0aa8e
gonriblun <gonriblun@alum.us.es>
2022-12-04 17:41:28 +0100

    move tokens

--- src/main/java/org/springframework/samples/petclinic/token/TokenService.java
+++ src/main/java/org/springframework/samples/petclinic/token/TokenService.java
@@ -1,10 +1,12 @@
 package org.springframework.samples.petclinic.token;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.PetrisBoard;
+import org.springframework.samples.petclinic.space.Space;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -34,6 +36,35 @@ public class TokenService {
         for (Token token : createdBoard.getTokens()){
             token.setPetrisBoard(createdBoard);
             this.save(token);
+        }
+    }
+
+    public void moveTokens(Space space1, Space space2, Integer numBacteriaToMove, String colour) {
+        
+        List<Token> listToken = this.tokenRepository.findPossibleTokensToChangeOfSpace(space1.getId(), colour);
+
+        while(numBacteriaToMove!=0){
+            Token tokenToChange = listToken.get(numBacteriaToMove-1);
+            tokenToChange.setSpace(space2);
+            tokenToChange.setHasBeenUsed(true);
+            this.save(tokenToChange);
+            numBacteriaToMove--;
+        }
+
+        List<Token> tokensToOrderSpace1 = this.tokenRepository.findTokensOfSpace(space1.getId(), colour);
+        Integer numTokensSpace1 = tokensToOrderSpace1.size();
+        List<Token> tokensToOrderSpace2 = this.tokenRepository.findTokensOfSpace(space2.getId(), colour);
+        Integer numTokensSpace2 = tokensToOrderSpace2.size();
+
+        for (Token token :  tokensToOrderSpace1){
+            token.setPositionInSpace(numTokensSpace1);
+            this.save(token);
+            numTokensSpace1++;
+        }
+        for (Token token :  tokensToOrderSpace2){
+            token.setPositionInSpace(numTokensSpace2);
+            this.save(token);
+            numTokensSpace2++;
         }
     }
 

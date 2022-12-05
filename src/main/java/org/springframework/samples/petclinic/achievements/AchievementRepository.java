1d3343f07f51af1174d73fead6809190724765e9
Jaime García García <jaigargar1@alum.us.es>
2022-12-05 17:16:32 +0100

    achievements1

--- src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
+++ src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
@@ -1,11 +1,17 @@
 package org.springframework.samples.petclinic.achievements;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AchievementRepository extends CrudRepository<Achievement, Integer> {
+    
+    List<Achievement> findAll();
+    
+    @Query("SELECT a FROM Achievement a WHERE a.id = ?1")
+    Achievement findAchievementByid(Integer id);
 
-    
-    
 }
--- src/main/java/org/springframework/samples/petclinic/achievements/AchievementService.java
+++ src/main/java/org/springframework/samples/petclinic/achievements/AchievementService.java
@@ -1,10 +1,30 @@
 package org.springframework.samples.petclinic.achievements;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AchievementService {
 
-    AchievementRepository repo;
+    AchievementRepository achievementRepository;
+
+    @Autowired
+	public AchievementService(AchievementRepository achievementRepository) {
+		this.achievementRepository = achievementRepository;
+	}
+
+    public List<Achievement> getAllAchievements(){
+        return achievementRepository.findAll();
+    }
+
+    public Achievement getAchievementById(Integer id){
+        return achievementRepository.findAchievementByid(id);        
+    }
+
+    public Achievement save(Achievement a){
+        return achievementRepository.save(a);
+    }
     
 }
